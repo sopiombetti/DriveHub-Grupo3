d090ffd0acea039a490bf258db975d730e760023
federicogonzalez1988 <federico.gon88@gmail.com>
2025-09-09 11:47:53 -0300

    creacion de la clase abstracta Estado

---
+++ src/estado.ts
@@ -0,0 +1,15 @@
+abstract class Estado {
+    protected tipoEstado: string;
+
+    constructor(tipoEstado: string) {
+        this.tipoEstado = tipoEstado;
+    }
+
+    public getTipoEstado(): string {
+        return this.tipoEstado;
+    }
+
+    public setTipoEstado(tipoEstado: string): void {
+        this.tipoEstado = tipoEstado;
+    }
+}
