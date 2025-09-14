6fc78204b620613c9bad55f3e919b6c522e00332
sopiombetti <sopiombetti@gmail.com>
2025-09-14 20:56:20 -0300

    Agregando constructor en clase Compacto

--- src/compacto.ts
+++ src/compacto.ts
@@ -1,6 +1,13 @@
+import Estado from "./estado";
 import Vehiculo from "./vehiculo";
 
 export default class Compacto extends Vehiculo{
+
+    constructor(matricula:string, estado:Estado, kilometraje:number){
+        super(matricula, estado, kilometraje);
+        this.tarifaBase = 30;
+        this.valorCargoExtra = 0.15;
+    }
 
     condicionCargosExtra(kmTotales: number, diasTotales: number): boolean {
         let promedioKilometrosPorDia = kmTotales/diasTotales;
