b326c32de58fe6f3b03dc60d40d981e72e43ce08
juanivp <jivp1991@gmail.com>
2025-10-08 00:01:25 -0300

    documentacion-clase-SUV

--- src/vehiculos/suv.ts
+++ src/vehiculos/suv.ts
@@ -1,8 +1,18 @@
 import Estado from "../estados/estado";
 import Vehiculo from "./vehiculo";
-
+/**
+Representa un vehiculo de tipo SUV con sus atributos particulares.
+*/
 export default class SUV extends Vehiculo{
-
+    /**
+     * crea un vehiculo de tipo SUV
+     * @param {string} matricula -  
+     * @param {Estado} estado - estado del vehiculo 
+     * @param {number} kilometraje - cantidad de km que marca el cuentakilometros 
+     * @param {number} tarifaBase - tarifa base por alquilar un SUV. Igual para todas las instancias
+     * @param {number} valorCargoExtra - cargo extra por alquilar un SUV. Igual para todas las instancias
+     * @param {number} tarifaBase - cargo extra por concepto de seguro. Igual para todas las instancias
+     */
     constructor(matricula:string, estado:Estado, kilometraje:number){
         super(matricula, estado, kilometraje);
         this.tarifaBase = 80;
