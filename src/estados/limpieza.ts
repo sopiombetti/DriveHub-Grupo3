83467099937a1cb2b490749c8f4e4de3540949e8
gastonmonsalvo <gastonmonsalvo@yahoo.com>
2025-11-01 12:07:17 -0300

    actualizacion-clase-limpieza

--- src/estados/limpieza.ts
+++ src/estados/limpieza.ts
@@ -1,7 +1,29 @@
-import Estado from "./estado";
+import Vehiculo from "../vehiculos/vehiculo";
+import Disponible from "./disponible";
+import IEstado from "./estado";
 
-export default class Limpieza extends Estado {
-    constructor() {
-        super("Limpieza");
+export default class Limpieza implements IEstado {
+   constructor(private vehiculo : Vehiculo){}
+
+    public alquilar() {
+        // alquilerException a implementar
+        throw new Error();
+    }  
+    public ponerEnMantenimiento(){
+        //mantenimientoException a implementar
+        throw new Error();
+    }
+
+    public ponerEnLimpieza(){
+        //limpiezaException a implementar
+        throw new Error();
+    }
+
+    public ponerDisponible(){
+        this.vehiculo.cambiarEstado(new Disponible(this.vehiculo));
+    }
+
+    public puedeSerAlquilado(){
+        return false;
     }
 }
