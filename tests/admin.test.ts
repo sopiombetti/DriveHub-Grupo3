3cba592a477da5d782c18a11312aaecc431af772
sopiombetti <sopiombetti@gmail.com>
2025-11-05 10:11:32 -0300

    correcciones clase admin-test

--- tests/admin.test.ts
+++ tests/admin.test.ts
@@ -1,14 +1,11 @@
 import Admin from '../src/admin';
 import Cliente from '../src/cliente';
-import Estado from '../src/estados/estado';
 import Vehiculo from '../src/vehiculos/vehiculo';
 
-class MockEstado extends Estado{
-}
 
 class MockVehiculo extends Vehiculo{
-    constructor(matricula:string, estado: MockEstado, kilometraje:number) {
-        super(matricula, estado, kilometraje);
+    constructor(matricula:string, kilometraje:number) {
+        super(matricula, kilometraje);
     }
     condicionCargosExtra(kmTotales: number, diasTotales: number): boolean {
         return true;
@@ -26,18 +23,10 @@ describe('Tests clase Admin', () => {
     });
 
     it('Chequear disponibilidad (true)', () => {
-        const estado = new MockEstado('Disponible')
-        const vehiculoDisponible = new MockVehiculo("000", estado, 10);
+        const vehiculoDisponible = new MockVehiculo("000", 10);
         const resultado = admin.chequearDisponibilidad(vehiculoDisponible);
         expect(resultado).toBe(true);
     });
-
-    it('Chequear disponibilidad (false)', () => {
-        const estado = new MockEstado('Mantenimiento')
-        const vehiculoDisponible = new MockVehiculo("000", estado, 10);
-        const resultado = admin.chequearDisponibilidad(vehiculoDisponible);
-        expect(resultado).toBe(false);
-    });  
 
     it('Agregar un cliente nuevo al array', () => {
         const cliente = new MockCliente('Sofia', '123', '@mail');
@@ -65,23 +54,20 @@ describe('Tests clase Admin', () => {
     });
 
     it('Agregar un vehiculo nuevo al array', () => {
-        const estado = new MockEstado('Disponible')
-        const vehiculo = new MockVehiculo("000", estado, 10);
+        const vehiculo = new MockVehiculo("000", 10);
         admin.agregarVehiculo(vehiculo);
         expect(admin['vehiculos']).toContain(vehiculo);
     });
 
     it('Vehiculo ya existe en el array', () => {
-        const estado = new MockEstado('Disponible')
-        const vehiculo1 = new MockVehiculo("000", estado, 10);
-        const vehiculo2 = new MockVehiculo("000", estado, 10);
+        const vehiculo1 = new MockVehiculo("000", 10);
+        const vehiculo2 = new MockVehiculo("000", 10);
         admin.agregarVehiculo(vehiculo1);
         expect(() => admin.agregarVehiculo(vehiculo2)).toThrow('El vehiculo ya se encuentra en el array');
     });
 
     it('Quitar un vehiculo del array', () => {
-        const estado = new MockEstado('Disponible')
-        const vehiculo = new MockVehiculo("000", estado, 10);
+        const vehiculo = new MockVehiculo("000", 10);
         admin.agregarVehiculo(vehiculo);
         admin.quitarVehiculo(vehiculo);
         expect(admin['vehiculos']).not.toContain(vehiculo);
