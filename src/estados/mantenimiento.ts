a57d4b93d0ee47e21dcc0c64623913cbeed252de
sopiombetti <sopiombetti@gmail.com>
2025-11-15 14:37:15 -0300

    documentación clase mantenimiento

--- src/estados/mantenimiento.ts
+++ src/estados/mantenimiento.ts
@@ -7,13 +7,25 @@ import IEstado from "./estado";
 export default class Mantenimiento implements IEstado {  
     constructor(private vehiculo : Vehiculo){}
 
+    /**
+    * Lanza una excepción al no poder ser alquilado.
+    * @throws {AlquilarException}
+    */
     public alquilar() {
         throw new AlquilarException("El vehiculo no se puede alquilar");
     }  
+
+    /**
+    * Lanza una excepción al no poder ir a mantenimiento.
+    * @throws {MantenimientoException}
+    */
     public ponerEnMantenimiento(){
         throw new MantenimientoException("El vehiculo no puede ser enviado a mantenimiento");
     }
 
+    /**
+    * Cambia de estado a Disponible.
+    */
     public ponerDisponible(){
         this.vehiculo.cambiarEstado(new Disponible(this.vehiculo));
     }
