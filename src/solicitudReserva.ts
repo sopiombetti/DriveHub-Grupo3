6c9fc5debb4582defb81bf9ec0fc4c335806c54c
sopiombetti <sopiombetti@gmail.com>
2025-11-15 15:11:48 -0300

    documentación clase solicitudReserva

--- src/solicitudReserva.ts
+++ src/solicitudReserva.ts
@@ -6,15 +6,34 @@ export default class SolicitudReserva {
     constructor( private cliente:Cliente ,private vehiculo: Vehiculo,private fechaInicio: Date,private fechaFin: Date) {
     }
 
+    /**
+     * Obtiene el vehículo solicitado.
+     * @returns {Vehiculo}
+     */
     getVehiculo(): Vehiculo {
         return this.vehiculo;
     }
+
+    /**
+     * Obtiene el cliente que solicita.
+     * @returns {Cliente}
+     */
     getCliente(): Cliente {
         return this.cliente;
     }
+
+    /**
+     * Obtiene la fecha de inicio solicitada.
+     * @returns {Date}
+     */
     getFechaInicio(): Date {
         return this.fechaInicio;
     }
+
+    /**
+     * Obtiene la fecha de fin solicitada.
+     * @returns {Date}
+     */
     getFechaFin(): Date {
         return this.fechaFin;
     }
