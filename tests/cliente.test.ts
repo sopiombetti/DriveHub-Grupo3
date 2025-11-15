1ccf424dec0be4cafbc54675dad8b51c493f34ef
federicogonzalez1988 <federico.gon88@gmail.com>
2025-11-15 08:12:46 -0300

    arreglos en test de clase cliente

--- tests/cliente.test.ts
+++ tests/cliente.test.ts
@@ -1,10 +1,10 @@
 import Cliente from '../src/cliente';
 import Vehiculo from '../src/vehiculos/vehiculo';
 import SolicitudReserva from '../src/solicitudReserva';
+import Admin from '../src/admin'; 
 
 
-jest.mock('../src/solicitudReserva');
-
+jest.mock('../src/admin'); 
 
 class MockVehiculo extends Vehiculo{
     constructor(matricula:string, kilometraje:number) {
@@ -17,26 +17,37 @@ class MockVehiculo extends Vehiculo{
 describe('Tests de Cliente', () => {
 
     let cliente: Cliente;
+    let mockAdmin: Admin; 
     let mockVehiculo: MockVehiculo;
-    const MockSolicitudReserva = SolicitudReserva as jest.MockedClass<typeof SolicitudReserva>;
+    const MockedAdmin = Admin as jest.MockedClass<typeof Admin>; 
 
     const FECHA_INICIO = new Date('2025-12-01');
     const FECHA_FIN = new Date('2025-12-10');
 
     beforeEach(() => {
         
-        MockSolicitudReserva.mockClear();
+        MockedAdmin.mockClear(); 
+        mockAdmin = new MockedAdmin(); 
 
-        cliente = new Cliente('Ana', '123', '@mail');
+        cliente = new Cliente('Ana', '123', '@mail', mockAdmin);
         mockVehiculo = new MockVehiculo('ABC-123', 0);
     });
 
-    it('debería llamar al constructor de SolicitudReserva con los datos correctos', () => {
-      
+    it('debe crear una solicitud y enviarla al Admin con los datos correctos', () => {
+
         cliente.generarSolicitud(mockVehiculo, FECHA_INICIO, FECHA_FIN);
 
-        expect(MockSolicitudReserva).toHaveBeenCalledTimes(1);
 
-        expect(MockSolicitudReserva).toHaveBeenCalledWith(cliente, mockVehiculo, FECHA_INICIO, FECHA_FIN);
+        expect(mockAdmin.generarReserva).toHaveBeenCalledTimes(1);
+        
+
+        const solicitudPasada: SolicitudReserva = (mockAdmin.generarReserva as jest.Mock).mock.calls[0][0];
+
+
+        expect(solicitudPasada).toBeInstanceOf(SolicitudReserva);
+        expect(solicitudPasada.getCliente()).toBe(cliente);
+        expect(solicitudPasada.getVehiculo()).toBe(mockVehiculo);
+        expect(solicitudPasada.getFechaInicio()).toBe(FECHA_INICIO);
+        expect(solicitudPasada.getFechaFin()).toBe(FECHA_FIN);
     });
 });
