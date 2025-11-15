36301128141bb717beb1ea53510bf03826e0a127
sopiombetti <sopiombetti@gmail.com>
2025-11-15 14:39:27 -0300

    documentación clases temporadas

--- src/temporadas/temporadaBaja.ts
+++ src/temporadas/temporadaBaja.ts
@@ -1,6 +1,11 @@
 import Temporada from "./temporada";
 
 export default class TemporadaBaja implements Temporada{
+    
+    /**
+    * Devuelve el porcentaje que varía la tarifa según la temporada.
+    * @returns {number}
+    */
     public getPorcentajeTarifa(): number{
         return 0.9;
     }
