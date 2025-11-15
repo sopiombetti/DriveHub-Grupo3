a199c90e7beca30a8863e84a8805f39c55b37e4d
sopiombetti <sopiombetti@gmail.com>
2025-11-15 15:02:01 -0300

    documentación clase calculadoraTarifa

--- src/calculadoraTarifa.ts
+++ src/calculadoraTarifa.ts
@@ -5,7 +5,7 @@ Clase que calcula la tarifa que el cliente paga por la reserva.
 export default class CalculadoraTarifa {
 
 	/**
-     * metodo estatico que calcula la tarifa final de una reserva en base a los kilometros recorridos, los dias y los cargos del tipo de vehiculo.
+     * método que calcula la tarifa final de una reserva en base a los kilometros recorridos, los dias, los cargos del tipo de vehiculo y la temporada.
      * @param {Reserva} reserva a tarifar
 	 * @returns {number} - monto a pagar por el cliente
      */
