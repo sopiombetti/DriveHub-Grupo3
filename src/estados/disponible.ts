82648c575629caa5f71ba1b83a961de24bc1c63c
sopiombetti <sopiombetti@gmail.com>
2025-11-15 14:35:27 -0300

    documentación clase disponible

--- src/estados/disponible.ts
+++ src/estados/disponible.ts
@@ -4,16 +4,28 @@ import IEstado from "./estado";
 import Mantenimiento from "./mantenimiento";
 
 export default class Disponible implements IEstado {
- constructor(private vehiculo : Vehiculo){}
+    
+    constructor(private vehiculo : Vehiculo){}
 
+    /**
+    * Cambia de estado a Alquilado y le suma al vehiculo un alquiler.
+    */
     public alquilar() {
         this.vehiculo.cambiarEstado(new Alquilado(this.vehiculo));
         this.vehiculo.sumarAlquiler();
     }  
+
+    /**
+    * Cambia de estado a Mantenimiento.
+    */
     public ponerEnMantenimiento(){
         this.vehiculo.cambiarEstado(new Mantenimiento(this.vehiculo));
     }
 
+    /**
+    * Lanza una excepción al no poder cambiar a Disponible.
+    * @throws {Error}
+    */
     public ponerDisponible(){
         throw new Error("El vehiculo ya se encuentra disponible");
     }
